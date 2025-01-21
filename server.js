64ef91f91582c1242566a28ae8cdd21c6ea87b15
symonmuchemi <simonkenya846@gmail.com>
2025-01-22 01:04:49 +0300

    feat: creates connection to MongoDB

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 const bootcamps = require('./routes/bootcamps');
 
 dotenv.config({ path: './config/config.env' });
+
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
