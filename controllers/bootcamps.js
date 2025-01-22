bbcd24743905ac2c23cba5570123bd7d250fe696
symonmuchemi <simonkenya846@gmail.com>
2025-01-22 16:22:12 +0300

    update: create ErrorHandler class to handle error gracefully

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,4 +1,5 @@
 const Bootcamp = require('../models/Bootcamp');
+const ErrorResponse = require('../utils/errorResponse');
 
 // @desc    gets all bootcamps
 // @route   GET /api/v1/bootcamps
@@ -28,14 +29,14 @@ exports.getBootcamp = async (req, res, next) => {
     const bootcamp = await Bootcamp.findById(req.params.id);
 
     if (!bootcamp) {
-      return res.status(400).json({ success: false });
+      return next(new ErrorResponse(`Bootcamp with id: ${req.params.id} not found`, 404));
     }
     res.status(200).json({
       success: true,
       data: bootcamp,
     });
   } catch (error) {
-    return next(error);
+    next(next(new ErrorResponse(`Bootcamp with id: ${req.params.id} not found`, 404)));
   }
 };
 
--- middleware/error.js
+++ middleware/error.js
@@ -1,7 +1,7 @@
 const errorHandler = (error, req, res, next) => {
   console.log("Error is caught by the middleware!".red.bold);
 
-  res.status(500).json({
+  res.status(error.statusCode || 500).json({
     success: false,
     error: error.message,
   });
