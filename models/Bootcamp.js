c6282fd0bc12a03fb1ef1851acb78ec545bc5413
symonmuchemi <simonkenya846@gmail.com>
2025-01-29 19:08:00 +0300

    feat: add geocoding functionality to bootcamp model

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const geocoder = require('../utils/geocoder');
 
 const bootcampSchema = new mongoose.Schema({
   name: {
@@ -101,6 +102,23 @@ const bootcampSchema = new mongoose.Schema({
 
 bootcampSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+bootcampSchema.pre('save', async function (next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: 'Point',
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].state,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode,
+  };
+
+  this.address = undefined;
   next();
 });
 
