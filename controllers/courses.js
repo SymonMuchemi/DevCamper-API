260e10a443aa935631699b2e10713ee8a6819f8f
symonmuchemi <simonkenya846@gmail.com>
2025-02-15 18:56:09 +0300

    feat: implement advanced results middleware for course retrieval

--- controllers/courses.js
+++ controllers/courses.js
@@ -7,24 +7,7 @@ const Bootcamp = require('../models/Bootcamp');
 // @route   GET /api/v1/courses
 // @access  Public
 exports.getCourses = asyncHandler(async (req, res, next) => {
-  let queury;
-
-  if (req.params.bootcampId) {
-    query = Course.find({ bootcamp: req.params.bootcampId });
-  } else {
-    query = Course.find().populate({
-      path: 'bootcamp',
-      select: 'name description',
-    });
-  }
-
-  const courses = await query;
-
-  res.status(200).json({
-    success: true,
-    count: courses.length,
-    data: courses,
-  });
+  res.status(200).json(res.advancedResults);
 });
 
 // @desc    gets a single course
